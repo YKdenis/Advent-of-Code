aa3214b7aeec477fc08114d00b87005f6db37fb0
Denis <denis.inghelbrecht@outlook.be>
2022-12-06 16:33:56 +0100

    finishing up 4th puzzle

--- 2022/four/main.js
+++ 2022/four/main.js
@@ -3,31 +3,27 @@ const fs = require('fs');
 fs.readFile('input.txt', (err, data) => {
 	if (err) throw err;
 	const pairs = data.toString().split('\n');
-	pairs.reduce((amountOfPairs, pair) => {
+	const duplicatedEfforts = pairs.reduce((acc, pair) => {
 		const ranges = [
-			createRangeArray(splitPair(pair)[0]),
-			createRangeArray(splitPair(pair)[1]),
-		];
-		console.log(ranges);
-	});
+			createRangeArray(pair.split(',')[0]),
+			createRangeArray(pair.split(',')[1]),
+		].sort((a, b) => b.length - a.length);
+		// isRedundantPair(ranges) && acc++;
+		isOverlappingPair(ranges) && acc++;
+		return acc;
+	}, 0);
+	console.log(duplicatedEfforts);
 });
 
-const getHighestSectionRange = (pair) => {
-	const sections = [
-		getSectionRange(splitPair(pair)[0]),
-		getSectionRange(splitPair(pair)[1]),
-	];
-	return Number(sections[0] > sections[1]);
-};
+const isRedundantPair = (ranges) =>
+	ranges[1].every((sectionID) => ranges[0].includes(sectionID));
 
-const splitPair = (pair) => pair.split(',');
+const isOverlappingPair = (ranges) =>
+	ranges[1].find((sectionID) => ranges[0].includes(sectionID));
 
-const getSectionRange = (section) =>
-	parseInt(section.split('-')[1]) - parseInt(section.split('-')[0]);
-
-const createRangeArray = (section) => {
+const createRangeArray = (sectionID) => {
 	const rangeArray = [];
-	const range = section.split('-');
+	const range = sectionID.split('-');
 	for (let i = parseInt(range[0]); i < parseInt(range[1]) + 1; i++) {
 		rangeArray.push(i);
 	}
